6fe94aad0d1f541c92f65a4c99c17dda908c38df
Josh Mu <mu@joshmu.com>
2020-09-24 10:32:23 +1000

    :zap: global context added, updated tailwind

---
+++ src/context/globalContext.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from 'react'
+
+const globalContext = createContext({})
+
+export function GlobalProvider(props) {
+  const value = {}
+
+  return <globalContext.Provider value={value} {...props} />
+}
+
+export function useGlobalContext() {
+  return useContext(globalContext)
+}
