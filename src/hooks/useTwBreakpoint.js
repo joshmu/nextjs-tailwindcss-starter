f810b015f7007ff30c1c95763473adf1f8494f60
Josh Mu <mu@joshmu.com>
2020-10-23 21:56:30 +1000

    :hammer: fix useTwBreakpoint

--- src/hooks/useTwBreakpoint.js
+++ src/hooks/useTwBreakpoint.js
@@ -6,29 +6,49 @@ import twConfigFile from '@/root/tailwind.config'
 const twConfig = resolveConfig(twConfigFile)
 
 const useTwBreakpoint = () => {
-  const [breakpoint, setBreakpoint] = useState({ id: 'sm', size: 640 })
+  const [currentBreakpoint, setCurrentBreakpoint] = useState({
+    id: 'sm',
+    size: 640,
+  })
+  const [breakpoints, setBreakpoints] = useState({})
 
   useEffect(() => {
     const checkScreen = () => {
       const innerWidth = globalThis.window.innerWidth
       const screens = twConfig.theme.screens
-      const twBreakpoint = Object.entries(screens).find(([p, c]) => {
-        const breakpoint = +c.replace('px', '')
-        return innerWidth < breakpoint
-      })
 
-      const formatBreakpoint = {
-        id: twBreakpoint[0],
-        size: +twBreakpoint[1].replace('px', ''),
-      }
-      setBreakpoint(formatBreakpoint)
+      // grab values
+      const formattedBreakpoints = {}
+      Object.entries(screens)
+        // format {id, size}
+        .forEach(([key, val]) => {
+          formattedBreakpoints[key] = +val.replace('px', '')
+        })
+      setBreakpoints(formattedBreakpoints)
+
+      // largest first so 'find' gets max breakpoint
+      let twBreakpoint = Object.entries(formattedBreakpoints)
+        .map(([key, val]) => ({ id: key, size: val }))
+        // largest to smallest so find can grab the appropriate breakpoint
+        .sort((a, b) => b.size - a.size)
+        // min width breakpoints so grab when we go over
+        .find(breakpoint => {
+          return innerWidth > breakpoint.size
+        })
+
+      // if not found then it must be the less than the smallest breakpoint
+      if (!twBreakpoint) twBreakpoint = { id: 'xs', size: 0 }
+
+      setCurrentBreakpoint(twBreakpoint)
     }
     checkScreen()
 
     globalThis.window.addEventListener('resize', checkScreen)
     return () => globalThis.window.removeEventListener('resize', checkScreen)
   }, [])
-  return { breakpoint }
+  return { currentBreakpoint, BREAKPOINTS: breakpoints }
 }
 
 export default useTwBreakpoint
+
+// @example :  currentBreakpoint.size < BREAKPOINTS['md']
