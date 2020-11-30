3da73dc0e0be3d2af55d4438964927dcd6a0b147
Josh Mu <mu@joshmu.com>
2020-11-30 16:02:42 +1000

    chore: remove redundant tw expiremental config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,39 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   purge: [
-    './pages/**/*.{ts,tsx, js,jsx}',
-    './src/components/**/*.{ts,tsx,js,jsx}',
+    "./pages/**/*.{ts,tsx, js,jsx}",
+    "./src/components/**/*.{ts,tsx,js,jsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        themeText: 'var(--text)',
-        themeBg: 'var(--background)',
-        themeAccent: 'var(--accent)',
+        themeText: "var(--text)",
+        themeBg: "var(--background)",
+        themeAccent: "var(--accent)",
       },
       maxHeight: {
-        xs: '20rem',
-        sm: '24rem',
-        md: '28rem',
-        lg: '32rem',
-        xl: '36rem',
-        '2xl': '42rem',
-        '3xl': '48rem',
-        '4xl': '56rem',
-        '5xl': '64rem',
-        '6xl': '72rem',
+        xs: "20rem",
+        sm: "24rem",
+        md: "28rem",
+        lg: "32rem",
+        xl: "36rem",
+        "2xl": "42rem",
+        "3xl": "48rem",
+        "4xl": "56rem",
+        "5xl": "64rem",
+        "6xl": "72rem",
       },
     },
   },
   variants: {},
   plugins:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === "production"
       ? // production
         []
       : // development
-        [require('tailwindcss-debug-screens')],
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  experimental: {
-    uniformColorPalette: true, // color shades are similar brightness
-    extendedSpacingScale: true, // 72, 80, 96
-    extendedFontSizeScale: true, // 7xl, 8xl, 9xl
-  },
-}
+        [require("tailwindcss-debug-screens")],
+};
